1cba61ac1027038ef9d8f575d3e311762fecc63e
kylvowray6 <kylvowray6@gmail.com>
2023-08-01 13:21:38 -0700

    mkd

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,15 +9,20 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+    listint_t *new;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
+    do
+    {
+        new = malloc(sizeof(listint_t));
+        if (!new)
+            break;
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
+        new->n = n;
+        new->next = *head;
+        *head = new;
 
-	return (new);
+        return new;
+    } while (0);
+
+    return NULL;
 }
